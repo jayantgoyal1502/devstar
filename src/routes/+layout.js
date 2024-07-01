5fcf745db45c3a1b49be99635bbd9c4977d539ef
desing raj <desing85131@gmail.com>
2024-07-01 21:22:27 +0530

    bifurcation(noun-verb)
    i have added the bifurcation functionality in the code which tanvi had provided

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,7 @@
 import tools from './tools.json';
 
 // @ts-ignore
-export async function load({route, url}) {
+export async function load({ route, url }) {
     return {
         tools: tools,
         meta: getMeta(route, url)
@@ -11,13 +11,27 @@ export async function load({route, url}) {
 // @ts-ignore
 function getMeta(route, url) {
     if (route.id && route.id.includes("(tools)")) {
+        // Remove leading slash from pathname
+        const pathKey = url.pathname.replace("/", "");
+        
         // @ts-ignore
-        let tool = tools[url.pathname.replace("/","")];
+        const tool = tools[pathKey];
+
+        if (!tool) {
+            console.error(`Tool not found for path: ${url.pathname}`);
+            return {
+                title: 'Random Word Generator',
+                description: 'It generates random words.',
+                contributors: []
+            };
+        }
+
         return {
             title: tool.name,
             description: tool.description,
             contributors: tool.contributors
-        }
+        };
     }
+
     return 0;
 }
