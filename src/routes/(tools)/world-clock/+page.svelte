28da488de9846ecc9c303e090a0c5150f9c38f7b
ILYAS SAYYED <mychoicesayyed@gmail.com>
2024-07-03 20:26:14 +0530

    updated project route

--- src/routes/(tools)/world-clock/+page.svelte
+++ src/routes/(tools)/world-clock/+page.svelte
@@ -4,6 +4,7 @@
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
+	<h1 class="text-white">World Clock</h1>
 </div>
 
 <style>
